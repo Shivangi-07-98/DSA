bdb01cf925199d6a6e403077c53f2dcbeba2a46f
Shivangi Kumari <12shivangikumari@gmail.com>
2024-11-26 13:23:23 +0530

    added ques track

---
+++ Level 1/8.Recursion with Arraylists/GetStairPathsQ86.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class GetStairPathsQ86 {
+  public static void main (String [] args){
+    Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine();
+    ArrayList <String> ans = gsp(str); 
+    System.out.println(ans);
+  }
+
+  public static ArrayList<String> gsp(String str){
+    
+    return null;
+  }
+}
