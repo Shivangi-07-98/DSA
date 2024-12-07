240a52bf191c71c72a629fccc916ad218ff4a128
Shivangi Kumari <12shivangikumari@gmail.com>
2024-12-07 22:32:29 +0530

    added files

---
+++ Level 1/11.Time and Space Complexity/RadixSortQ.java
@@ -0,0 +1,17 @@
+// to find 1st 2nd 3rd 4th 5th and so on... smallest element
+import java.util.*; 
+public class RadixSortQ {
+  public static void main (String [] args){
+    Scanner scn = new Scanner (System.in);
+    int n = scn.nextInt(); //7
+    int [] arr = new int [n];
+    for(int i = 0; i < arr.length; i++){
+      arr[i] = scn.nextInt(); // 5 9 3 2 6 8 17
+    }
+
+    // 2 3 5 6 8 9 17
+
+
+
+  }
+}
