cddaa3dcc74fc6ba23af4430f50b0e84fbae0b56
Shivangi Kumari <12shivangikumari@gmail.com>
2025-04-21 20:01:14 +0530

    added mirror in GT

--- Level 1/16. Generic Tree/MirrorQ.java
+++ Level 1/16. Generic Tree/MirrorQ.java
@@ -20,9 +20,29 @@ public class MirrorQ {
     }
   }
 
+  public static void levelOrderLinewise(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    Queue<Node> cqueue = new ArrayDeque<Node>();
+    queue.add(root);
+
+    while (queue.size() > 0) {
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+
+      for (Node child : temp.children) {
+        cqueue.add(child);
+      }
+
+      if (queue.size() == 0) {
+        queue = cqueue;
+        cqueue = new ArrayDeque<>();
+        System.out.println();
+      }
+    }
+  }
+
   public static Node construct(int[] arr) {
     Node root = null;
-
     Stack<Node> st = new Stack<>();
 
     for (int i = 0; i < arr.length; i++) {
@@ -45,8 +65,7 @@ public class MirrorQ {
   }
 
   public static void mirror(Node node) {
-
-    for(Node child: node.children){
+    for (Node child : node.children) {
       mirror(child);
     }
     // Collections.reverse(node.children); // this is for arraylists
@@ -55,33 +74,49 @@ public class MirrorQ {
     int li = 0;
     int ri = node.children.size() - 1;
     while (li < ri) {
-        Node left = node.children.get(li);
-        Node right = node.children.get(ri);
-        node.children.set(li, right);
-        node.children.set(ri, left);
-        li++;
-        ri--;
+      Node left = node.children.get(li);
+      Node right = node.children.get(ri);
+      node.children.set(li, right);
+      node.children.set(ri, left);
+      li++;
+      ri--;
     }
-    
+
+  }
+
+  public static void myMirror(Node node) {
+    for (Node child : node.children) {
+      myMirror(child);
+    }
+
+    int a = 0;
+    int b = node.children.size() - 1;
+
+    while (a < b) {
+      Node left = node.children.get(a);
+      Node right = node.children.get(b);
+      node.children.set(a, right);
+      node.children.set(b, left);
+
+      a++;
+      b--;
+    }
+
   }
 
   public static void main(String[] args) throws Exception {
-    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    // int n = Integer.parseInt(br.readLine());
-    // int[] arr = new int[n];
-
-    // String[] values = br.readLine().split(" ");
-    // for (int i = 0; i < n; i++) {
-    //   arr[i] = Integer.parseInt(values[i]);
-    // }
-
     int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
-      -1 };
+        -1 };
 
     Node root = construct(arr);
-    display(root);
-    mirror(root);
-    display(root);
+    // display(root);
+    // mirror(root);
+    // display(root);
+
+    levelOrderLinewise(root);
+    // mirror(root);
+    myMirror(root);
+    levelOrderLinewise(root);
   }
 
 }
