d0508fc35a2a12d8ed17ec67adaa5100b53ea627
Shivangi Kumari <12shivangikumari@gmail.com>
2025-05-03 22:54:34 +0530

    added Size Sum Maximum Height of a binary tree

--- Level 1/17. Binary Tree/SizeSumMaximumHeightQ.java
+++ Level 1/17. Binary Tree/SizeSumMaximumHeightQ.java
@@ -6,19 +6,32 @@ public class SizeSumMaximumHeightQ {
         int data;
         Node left;
         Node right;
+
+        // Node(int data, Node left, Node right){
+        // this.data = data;
+        // this.left = left;
+        // this.right = right;
+        // }
     }
 
     public static class Pair {
         Node node;
         int state;
+
+        // Pair(Node node, int state){
+        // this.node = node;
+        // this.state = state;
+        // }
     }
 
+    // create stack of pair
     public static Node construct(Integer[] arr) {
         Stack<Pair> stack = new Stack<>();
 
         Node root = new Node();
         root.data = arr[0];
 
+        // main lines
         Pair rootp = new Pair();
         rootp.node = root;
         rootp.state = 1;
@@ -67,11 +80,20 @@ public class SizeSumMaximumHeightQ {
         return root;
     }
 
+    // leftNode parentNode rightNode
+    // 25 50 75
+    // 12 25 37
+    // 12
+    // 30 37
+    // 30
+    // 62 75 87
+    // 62 70
+    // 70
+    // 87
     public static void display(Node node) {
         if (node == null) {
             return;
         }
-
         String str = " <- " + node.data + " -> ";
         String lcstr = node.left == null ? ".." : "" + node.left.data;
         String rcstr = node.right == null ? ".." : "" + node.right.data;
@@ -81,28 +103,66 @@ public class SizeSumMaximumHeightQ {
         display(node.right); // will print the entire right tree
     }
 
+    // dry run by making stack its easier
+    // total elements without including null
+    public static int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int ls = size(node.left); // 1
+        int rs = size(node.right); // 2
+        int size = ls + rs + 1; // 3
+        return size; // 4
+    }
 
+    public static int sum(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int lsum = sum(node.left);
+        int rsum = sum(node.right);
+        int sum = lsum + rsum + node.data;
+        return sum;
+    }
 
+    public static int max(Node node) {
+        if (node == null) {
+            return Integer.MIN_VALUE;
+        }
+        int lmax = max(node.left);
+        int rmax = max(node.right);
+        int max = Math.max(node.data, Math.max(lmax, rmax));
+        return max;
+    }
+
+    public static int height(Node node) {
+        if (node == null) {
+            return -1; // edges -1, nodes 0
+        }
+        int lh = height(node.left);
+        int rh = height(node.right);
+        int height = Math.max(lh, rh) + 1;
+        return height;
+    }
 
     public static void main(String[] args) {
-
         Integer[] arr = new Integer[] { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null,
                 87, null, null };
         Node root = construct(arr);
         // display(root);
 
         int size = size(root);
-        System.out.println(size);
+        System.out.println("size " + size);
 
         int sum = sum(root);
-        System.out.println(sum);
+        System.out.println("sum " + sum);
 
         int max = max(root);
-        System.out.println(max);
+        System.out.println("max " + max);
 
         int height = height(root);
-        System.out.println(height);
-        
+        System.out.println("height " + height);
+
     }
 
 }
