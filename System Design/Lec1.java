7173b4f2c31d3e73c75082ec9e7e860733b1be15
Shivangi Kumari <12shivangikumari@gmail.com>
2025-05-19 19:01:57 +0530

    added lec2

--- System Design/Lec1.java
+++ System Design/Lec1.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 public class Lec1 {
 
-  public static class Person{
+  public static class BankAccount{ // static use 1
     
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) { // static use 2
     
   }
 }
