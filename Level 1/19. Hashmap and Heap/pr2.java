077ff4c04f797aea8dcbb28f54ae9099004dc0e3
Shivangi Kumari <12shivangikumari@gmail.com>
2025-08-05 16:47:12 +0530

    added hashmap intro

---
+++ Level 1/19. Hashmap and Heap/pr2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class pr2{
+
+
+
+  public static void main(String[] args) throws Exception{
+
+  }
+
+}
+
+// int vs Integer
+// int(primitive integer)(it is made on stack)
+// Integer(class integer)(it can store null)(as it is made on heap and address is stored on stack)
