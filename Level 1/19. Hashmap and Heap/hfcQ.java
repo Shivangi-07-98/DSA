2a041008736524e7d43d9ec0c74a20ffe1faefe8
Shivangi Kumari <12shivangikumari@gmail.com>
2025-08-05 19:12:59 +0530

    added highest frequency character in hashmap

---
+++ Level 1/19. Hashmap and Heap/hfcQ.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class hfcQ {
+  
+
+
+
+  public static void main(String[] args) throws Exception{
+    
+    Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine(); // abcdefabcdefabcdefaabbbeeeeeef
+
+    HashMap<Character, Integer> fmap = new HashMap<>();
+
+    // System.out.println(str.length());
+    for(int i = 0; i < str.length(); i++){
+      char ch = str.charAt(i); 
+      // System.out.print(ch + " ");
+
+      if(fmap.containsKey(ch) == false){
+        fmap.put(ch, 1);
+      }
+      else{
+        int oldf = fmap.get(ch);
+        int newf = oldf + 1;
+        fmap.put(ch, newf);
+      }
+
+    }
+
+    // approach 1
+    Set<Character> keys = fmap.keySet();
+    char maxChar = ' ';
+    int maxFreq = 0;
+    
+    for(char key : keys){
+      int freq = fmap.get(key);
+      if(freq > maxFreq){
+        maxFreq = freq;
+        maxChar = key;
+        // System.out.println(maxChar + " " + maxFreq);
+      }
+    }
+    System.out.println(maxChar + " " + maxFreq);
+
+    
+    // approach 2
+    char mfch = str.charAt(0);
+    for(int i = 1; i < str.length(); i++){
+      char ch = str.charAt(i);
+
+      if(fmap.get(ch) > fmap.get(mfch)){
+        mfch = ch;
+      }
+    }
+    System.out.println(mfch);
+
+  }
+}
--- Level 1/19. Hashmap and Heap/pr3Intro.java
+++ Level 1/19. Hashmap and Heap/pr3Intro.java
@@ -8,7 +8,7 @@ public class pr3Intro{
   public static void main(String[] args) throws Exception{
     HashMap<String, Integer> pmap = new HashMap<>();
 
-    // add
+    // add O(1) time complexity
     // if key is not present then it adds 
     pmap.put("India", 130);
     pmap.put("China", 200);
@@ -16,12 +16,12 @@ public class pr3Intro{
     pmap.put("Utopia", 0);
     System.out.println(pmap);
 
-    // update
+    // update O(1) time complexity
     // if key is already present then it updates 
     pmap.put("Aus", 51);
     System.out.println(pmap);
 
-    // get
+    // get O(1) time complexity
     int ipop = pmap.get("India");
     System.out.println(ipop);
 
@@ -29,9 +29,11 @@ public class pr3Intro{
     Integer uspop = pmap.get("Us"); // key is not present so gives null 
     System.out.println(uspop);
 
+    // O(1) time complexity
     System.out.println(pmap.containsKey("Us"));
     System.out.println(pmap.containsKey("India"));
 
+    // O(n) time complexity
     // to get all keys
     Set<String> keys = pmap.keySet();
     for(String key: keys){
