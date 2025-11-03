1a9862e82098767b8ca3095c9f672f2cea54c775
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-04 04:20:46 +0530

    climb stairs

--- Level 2/starter/Dynamic Programming and Greedy/Q2_ClimbStairs.java
+++ Level 2/starter/Dynamic Programming and Greedy/Q2_ClimbStairs.java
@@ -9,6 +9,7 @@ public class Q2_ClimbStairs {
     
     System.out.println(climbStairs(n));
     System.out.println(climbStairs_memo(n, storage));
+    System.out.println(climbStairs_tab(n));
   }
 
   // Recursive solution
@@ -48,6 +49,26 @@ public class Q2_ClimbStairs {
 
     storage[n] = total;
     return total;
+  }
+
+  // Tabulation solution
+  public static int climbStairs_tab(int n) {
+    int[] strg = new int[n + 1];
+    strg[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+      if (i - 1 >= 0) {
+        strg[i] += strg[i - 1];
+      }
+      if (i - 2 >= 0) {
+        strg[i] += strg[i - 2];
+      }
+      if (i - 3 >= 0) {
+        strg[i] += strg[i - 3];
+      }
+    }
+
+    return strg[n];
   }
   
 }
