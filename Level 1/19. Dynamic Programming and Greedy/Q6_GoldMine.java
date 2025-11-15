99876f5373436e39d88bd02d3039f97e124da227
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-15 23:44:30 +0530

    gold mine

--- Level 1/19. Dynamic Programming and Greedy/Q6_GoldMine.java
+++ Level 1/19. Dynamic Programming and Greedy/Q6_GoldMine.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 public class Q6_GoldMine {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int m = scn.nextInt();
+    int n = scn.nextInt(); // 6
+    int m = scn.nextInt(); // 6
     int[][] mine = new int[n][m];
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
@@ -12,113 +13,56 @@ public class Q6_GoldMine {
       }
     }
 
-    int[][] storage = new int[n][m];
-    for (int i = 0; i < n; i++) {
-      Arrays.fill(storage[i], -1);
-    }
-    
-    int maxGold = 0;
-    for (int i = 0; i < n; i++) {
-      maxGold = Math.max(maxGold, goldMine(i, 0, mine));
-    }
-    System.out.println(maxGold);
-
-    maxGold = 0;
-    for (int i = 0; i < n; i++) {
-      maxGold = Math.max(maxGold, goldMine_memo(i, 0, mine, storage));
-    }
-    System.out.println(maxGold);
-    
     System.out.println(goldMine_tab(n, m, mine));
-  }
-
-  // Recursive solution
-  public static int goldMine(int r, int c, int[][] mine) {
-    if (c == mine[0].length - 1) {
-      return mine[r][c];
-    }
-
-    int maxGold = mine[r][c];
-    int nextGold = 0;
-
-    if (r - 1 >= 0) {
-      nextGold = Math.max(nextGold, goldMine(r - 1, c + 1, mine));
-    }
-    nextGold = Math.max(nextGold, goldMine(r, c + 1, mine));
-    if (r + 1 < mine.length) {
-      nextGold = Math.max(nextGold, goldMine(r + 1, c + 1, mine));
-    }
-
-    return maxGold + nextGold;
-  }
-
-  // Memoized solution
-  public static int goldMine_memo(int r, int c, int[][] mine, int[][] storage) {
-    if (c == mine[0].length - 1) {
-      return mine[r][c];
-    }
-
-    if (storage[r][c] != -1) {
-      return storage[r][c];
-    }
-
-    int maxGold = mine[r][c];
-    int nextGold = 0;
-
-    if (r - 1 >= 0) {
-      nextGold = Math.max(nextGold, goldMine_memo(r - 1, c + 1, mine, storage));
-    }
-    nextGold = Math.max(nextGold, goldMine_memo(r, c + 1, mine, storage));
-    if (r + 1 < mine.length) {
-      nextGold = Math.max(nextGold, goldMine_memo(r + 1, c + 1, mine, storage));
-    }
-
-    storage[r][c] = maxGold + nextGold;
-    return storage[r][c];
   }
 
   // Tabulation solution
   public static int goldMine_tab(int n, int m, int[][] mine) {
     int[][] strg = new int[mine.length][mine[0].length];
 
-    for (int j = mine[0].length - 1; j >= 0; j--) {
-      for (int i = 0; i < mine.length; i++) {
-        if (j == mine[0].length - 1) {
+    for (int j = strg[0].length - 1; j >= 0; j--) { // col 5 4 3 2 1 0
+      for (int i = 0; i < strg.length; i++) { // row 0 1 2 3 4 5
+        // 2d array mai ulta travel krenge from right to left
+        //  0-5(5) 0-5(4) 0-5(3) 0-5(2) 0-5(1) 0-5(0) = 5 loops
+        if (j == strg[0].length - 1) { // col = 5
           strg[i][j] = mine[i][j];
-        } else if (i == 0) {
-          int maxNext = Math.max(strg[i][j + 1], strg[i + 1][j + 1]);
-          strg[i][j] = mine[i][j] + maxNext;
-        } else if (i == mine.length - 1) {
-          int maxNext = Math.max(strg[i][j + 1], strg[i - 1][j + 1]);
-          strg[i][j] = mine[i][j] + maxNext;
+        } else if (i == 0) { // row = 0
+          strg[i][j] = mine[i][j] + Math.max(strg[i][j + 1], strg[i + 1][j + 1]);
+        } else if (i == strg.length - 1) { // row = 5
+          strg[i][j] = mine[i][j] + Math.max(strg[i][j + 1], strg[i - 1][j + 1]);
         } else {
-          int maxNext = Math.max(strg[i][j + 1], Math.max(strg[i - 1][j + 1], strg[i + 1][j + 1]));
-          strg[i][j] = mine[i][j] + maxNext;
+          strg[i][j] = mine[i][j] + Math.max(strg[i][j + 1], Math.max(strg[i - 1][j + 1], strg[i + 1][j + 1]));
         }
+
       }
     }
 
-    int maxGold = 0;
-    for (int i = 0; i < mine.length; i++) {
-      maxGold = Math.max(maxGold, strg[i][0]);
+    int maxGold = strg[0][0];
+    for (int i = 1; i < strg.length; i++) { // loop in row
+      if (strg[i][0] > maxGold) {
+        maxGold = strg[i][0];
+      }
     }
 
     return maxGold;
   }
 
 }
+// ques is you can start from any row of col 0 and move forward with three moves
+// (right, diagonal up and down) and reach end col collecting max gold
+// movement is right, diagonal up, diagonal down
 
 /*
-Sample Input:
-6
-6
-0 1 4 2 8 2
-4 3 6 5 0 4
-1 2 4 1 4 6
-2 0 7 3 2 2
-3 1 5 9 2 4
-2 7 0 8 5 1
-
-Sample Output:
-33
-*/
+ * Sample Input:
+ * 6
+ * 6
+ * 0 1 4 2 8 2
+ * 4 3 6 5 0 4
+ * 1 2 4 1 4 6
+ * 2 0 7 3 2 2
+ * 3 1 5 9 2 4
+ * 2 7 0 8 5 1
+ * 
+ * Sample Output:
+ * 33
+ */
