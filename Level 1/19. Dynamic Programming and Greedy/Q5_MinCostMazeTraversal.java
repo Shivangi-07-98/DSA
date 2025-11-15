a0ea39da36977e5186f535c5f71b9757062d9aa9
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-15 12:10:03 +0530

    min cost in maze traversal

--- Level 1/19. Dynamic Programming and Greedy/Q5_MinCostMazeTraversal.java
+++ Level 1/19. Dynamic Programming and Greedy/Q5_MinCostMazeTraversal.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 public class Q5_MinCostMazeTraversal {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int m = scn.nextInt();
-    int[][] maze = new int[n][m];
+    int n = scn.nextInt(); // 6
+    int m = scn.nextInt(); // 6
+    int[][] maze = new int[n][m]; // 6 6
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         maze[i][j] = scn.nextInt();
       }
     }
-    
+
     System.out.println(minCostMaze_tab(n, m, maze));
   }
 
@@ -19,57 +20,41 @@ public class Q5_MinCostMazeTraversal {
   public static int minCostMaze_tab(int n, int m, int[][] maze) {
     int[][] strg = new int[n][m];
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (i == 0 && j == 0) {
+    for (int i = n - 1; i >= 0; i--) { // 5-0
+      for (int j = m - 1; j >= 0; j--) { // 5-0
+        // 2d array mai ulta travel krenge
+        if (i == n - 1 && j == m - 1) { // strg[5][5]
           strg[i][j] = maze[i][j];
-        } else if (i == 0) {
-          strg[i][j] = strg[i][j - 1] + maze[i][j];
-        } else if (j == 0) {
-          strg[i][j] = strg[i - 1][j] + maze[i][j];
-        } else {
-          strg[i][j] = Math.min(strg[i - 1][j], strg[i][j - 1]) + maze[i][j];
-        }
-      }
-    }
-
-    return strg[n - 1][m - 1];
-  }
-
-  // Tabulation solution pep
-  public static int minCostMaze_tab_pep(int n, int m, int[][] maze) {
-    int[][] strg = new int[n][m];
-
-    for (int i = n - 1; i >= 0; i--) {
-      for (int j = m - 1; j >= 0; j--) {
-        if (i == n - 1 && j == m - 1) {
-          strg[i][j] = maze[i][j];
-        } else if (i == n - 1) {
+        } else if (i == n - 1) { // row = 5
           strg[i][j] = strg[i][j + 1] + maze[i][j];
-        } else if (j == m - 1) {
+        } else if (j == m - 1) { // col = 5
           strg[i][j] = strg[i + 1][j] + maze[i][j];
         } else {
           strg[i][j] = Math.min(strg[i + 1][j], strg[i][j + 1]) + maze[i][j];
         }
+
       }
     }
 
     return strg[0][0];
   }
-  
+
 }
 
-/*
-Sample Input:
-6
-6
-0 1 4 2 8 2
-4 3 6 5 0 4
-1 2 4 1 4 6
-2 0 7 3 2 2
-3 1 5 9 2 4
-2 7 0 8 5 1
+// ques is arr[0][0] => arr[5][5] path travel krne ki min cost kya hai
+// movement is right and bottom only
 
-Sample Output:
-23
-*/
+/*
+ * Sample Input:
+ * 6
+ * 6
+ * 0 1 4 2 8 2
+ * 4 3 6 5 0 4
+ * 1 2 4 1 4 6
+ * 2 0 7 3 2 2
+ * 3 1 5 9 2 4
+ * 2 7 0 8 5 1
+ * 
+ * Sample Output:
+ * 23
+ */
