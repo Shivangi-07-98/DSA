cd8688ec5b6085e06f38bfa0774eda5828aa2c23
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-16 02:19:20 +0530

    target sum subsets

--- Level 1/19. Dynamic Programming and Greedy/Q7_TargetSumSubsets.java
+++ Level 1/19. Dynamic Programming and Greedy/Q7_TargetSumSubsets.java
@@ -1,32 +1,20 @@
 import java.util.*;
+
 public class Q7_TargetSumSubsets {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
+    int n = scn.nextInt(); // 5
     int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // 4 2 7 1 3
       arr[i] = scn.nextInt();
     }
-    int target = scn.nextInt();
+    int target = scn.nextInt(); // 10
 
-    Boolean[][] storage = new Boolean[n + 1][target + 1];
+    Boolean[][] storage = new Boolean[n + 1][target + 1]; // 6 11
     
-    System.out.println(targetSumSubsets(0, 0, arr, target));
     System.out.println(targetSumSubsets_memo(0, 0, arr, target, storage));
     System.out.println(targetSumSubsets_tab(arr, target));
-  }
-
-  // Recursive solution
-  public static boolean targetSumSubsets(int idx, int sum, int[] arr, int target) {
-    if (idx == arr.length) {
-      return sum == target;
-    }
-
-    boolean include = targetSumSubsets(idx + 1, sum + arr[idx], arr, target);
-    boolean exclude = targetSumSubsets(idx + 1, sum, arr, target);
-
-    return include || exclude;
   }
 
   // Memoized solution
@@ -50,8 +38,8 @@ public class Q7_TargetSumSubsets {
   public static boolean targetSumSubsets_tab(int[] arr, int target) {
     boolean[][] strg = new boolean[arr.length + 1][target + 1];
 
-    for (int i = 0; i <= arr.length; i++) {
-      for (int j = 0; j <= target; j++) {
+    for (int i = 0; i < strg.length; i++) {
+      for (int j = 0; j < strg[0].length; j++) {
         if (i == 0 && j == 0) {
           strg[i][j] = true;
         } else if (i == 0) {
@@ -59,34 +47,11 @@ public class Q7_TargetSumSubsets {
         } else if (j == 0) {
           strg[i][j] = true;
         } else {
-          if (strg[i - 1][j]) {
+          strg[i][j] = strg[i - 1][j];
+          int val = arr[i - 1];
+          if (j >= val && strg[i - 1][j - val]) {
             strg[i][j] = true;
-          } else if (j >= arr[i - 1] && strg[i - 1][j - arr[i - 1]]) {
-            strg[i][j] = true;
-          } else {
-            strg[i][j] = false;
           }
-        }
-      }
-    }
-
-    return strg[arr.length][target];
-  }
-
-  // Tabulation solution pep
-  public static boolean targetSumSubsets_tab_pep(int[] arr, int target) {
-    boolean[][] strg = new boolean[arr.length + 1][target + 1];
-
-    for (int i = 0; i <= arr.length; i++) {
-      for (int j = 0; j <= target; j++) {
-        if (i == 0 && j == 0) {
-          strg[i][j] = true;
-        } else if (i == 0) {
-          strg[i][j] = false;
-        } else if (j == 0) {
-          strg[i][j] = true;
-        } else {
-          strg[i][j] = strg[i - 1][j] || (j >= arr[i - 1] && strg[i - 1][j - arr[i - 1]]);
         }
       }
     }
@@ -95,6 +60,8 @@ public class Q7_TargetSumSubsets {
   }
   
 }
+// kya iss array ka koi subset aisa hai jiska sum 10 hai
+// each element can be included or excluded
 
 /*
 Sample Input:
@@ -105,3 +72,38 @@ Sample Input:
 Sample Output:
 true
 */
+
+// subset calculation formula = 2^n
+// All 32 subsets of [4, 2, 7, 1, 3]
+// 1. [_, _, _, _, _] = sum 0
+// 2. [_, _, _, _, 3] = sum 3
+// 3. [_, _, _, 1, _] = sum 1
+// 4. [_, _, _, 1, 3] = sum 4
+// 5. [_, _, 7, _, _] = sum 7
+// 6. [_, _, 7, _, 3] = sum 10**
+// 7. [_, _, 7, 1, _] = sum 8
+// 8. [_, _, 7, 1, 3] = sum 11
+// 9. [_, 2, _, _, _] = sum 2
+// 10. [_, 2, _, _, 3] = sum 5
+// 11. [_, 2, _, 1, _] = sum 3
+// 12. [_, 2, _, 1, 3] = sum 6
+// 13. [_, 2, 7, _, _] = sum 9
+// 14. [_, 2, 7, _, 3] = sum 12
+// 15. [_, 2, 7, 1, _] = sum 10**
+// 16. [_, 2, 7, 1, 3] = sum 13
+// 17. [4, _, _, _, _] = sum 4
+// 18. [4, _, _, _, 3] = sum 7
+// 19. [4, _, _, 1, _] = sum 5
+// 20. [4, _, _, 1, 3] = sum 8
+// 21. [4, _, 7, _, _] = sum 11
+// 22. [4, _, 7, _, 3] = sum 14
+// 23. [4, _, 7, 1, _] = sum 12
+// 24. [4, _, 7, 1, 3] = sum 15
+// 25. [4, 2, _, _, _] = sum 6
+// 26. [4, 2, _, _, 3] = sum 9
+// 27. [4, 2, _, 1, _] = sum 7
+// 28. [4, 2, _, 1, 3] = sum 10**
+// 29. [4, 2, 7, _, _] = sum 13
+// 30. [4, 2, 7, _, 3] = sum 16
+// 31. [4, 2, 7, 1, _] = sum 14
+// 32. [4, 2, 7, 1, 3] = sum 17
