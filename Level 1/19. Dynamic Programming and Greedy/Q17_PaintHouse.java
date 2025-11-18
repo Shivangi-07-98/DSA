7e2feb6e00d4df48b78e9254cfe6b5e183062492
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-18 23:18:50 +0530

    paint house

--- Level 1/19. Dynamic Programming and Greedy/Q17_PaintHouse.java
+++ Level 1/19. Dynamic Programming and Greedy/Q17_PaintHouse.java
@@ -1,114 +1,57 @@
 import java.util.*;
+
 public class Q17_PaintHouse {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int[][] costs = new int[n][3];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < 3; j++) {
-        costs[i][j] = scn.nextInt();
-      }
+    int n = scn.nextInt(); // 4
+    int[][] arr = new int[n][3]; // 4 3
+    for (int i = 0; i < arr.length; i++) {
+      arr[i][0] = scn.nextInt();
+      arr[i][1] = scn.nextInt();
+      arr[i][2] = scn.nextInt();
     }
 
-    Integer[][] storage = new Integer[n][3];
-    System.out.println(paintHouse(costs, 0, -1));
-    System.out.println(paintHouse_memo(costs, 0, -1, storage));
-    System.out.println(paintHouse_tab(costs));
-    System.out.println(paintHouse_optimized(costs));
-  }
-
-  // Recursive solution
-  public static int paintHouse(int[][] costs, int house, int prevColor) {
-    if (house == costs.length) {
-      return 0;
-    }
-
-    int minCost = Integer.MAX_VALUE;
-    for (int color = 0; color < 3; color++) {
-      if (color != prevColor) {
-        int cost = costs[house][color] + paintHouse(costs, house + 1, color);
-        minCost = Math.min(minCost, cost);
-      }
-    }
-
-    return minCost;
-  }
-
-  // Memoized solution
-  public static int paintHouse_memo(int[][] costs, int house, int prevColor, Integer[][] storage) {
-    if (house == costs.length) {
-      return 0;
-    }
-
-    int storageIdx = prevColor == -1 ? 0 : prevColor + 1;
-    if (prevColor != -1 && storage[house][prevColor] != null) {
-      return storage[house][prevColor];
-    }
-
-    int minCost = Integer.MAX_VALUE;
-    for (int color = 0; color < 3; color++) {
-      if (color != prevColor) {
-        int cost = costs[house][color] + paintHouse_memo(costs, house + 1, color, storage);
-        minCost = Math.min(minCost, cost);
-      }
-    }
-
-    if (prevColor != -1) {
-      storage[house][prevColor] = minCost;
-    }
-    return minCost;
-  }
-
-  // Tabulation solution
-  public static int paintHouse_tab(int[][] costs) {
-    int n = costs.length;
-    int[][] strg = new int[n][3];
-
-    for (int j = 0; j < 3; j++) {
-      strg[0][j] = costs[0][j];
-    }
-
-    for (int i = 1; i < n; i++) {
-      strg[i][0] = costs[i][0] + Math.min(strg[i - 1][1], strg[i - 1][2]);
-      strg[i][1] = costs[i][1] + Math.min(strg[i - 1][0], strg[i - 1][2]);
-      strg[i][2] = costs[i][2] + Math.min(strg[i - 1][0], strg[i - 1][1]);
-    }
-
-    return Math.min(Math.min(strg[n - 1][0], strg[n - 1][1]), strg[n - 1][2]);
-  }
-
-  // Space optimized solution
-  public static int paintHouse_optimized(int[][] costs) {
-    int n = costs.length;
-    int prev0 = costs[0][0];
-    int prev1 = costs[0][1];
-    int prev2 = costs[0][2];
-
-    for (int i = 1; i < n; i++) {
-      int curr0 = costs[i][0] + Math.min(prev1, prev2);
-      int curr1 = costs[i][1] + Math.min(prev0, prev2);
-      int curr2 = costs[i][2] + Math.min(prev0, prev1);
-
-      prev0 = curr0;
-      prev1 = curr1;
-      prev2 = curr2;
-    }
-
-    return Math.min(Math.min(prev0, prev1), prev2);
+    System.out.println(paintHouse_tab(arr));
   }
   
+  // Sample Input:
+  // 4
+  // 1 5 7 (house 1)
+  // 5 8 4 (house 2)
+  // 3 2 9 (house 3)
+  // 1 2 4 (house 4)
+  // Tabulation solution - Space optimized
+  public static int paintHouse_tab(int[][] arr) {
+    int red = arr[0][0];
+    int green = arr[0][1];
+    int blue = arr[0][2];
+
+    for (int i = 1; i < arr.length; i++) { // row = 1-3
+      int nred = Math.min(green, blue) + arr[i][0];
+      int ngreen = Math.min(red, blue) + arr[i][1];
+      int nblue = Math.min(green, red) + arr[i][2];
+
+      red = nred;
+      green = ngreen;
+      blue = nblue;
+    }
+
+    return Math.min(red, Math.min(green, blue));
+  }
+
 }
+// adjacent houses cannot have same color
+// minimize cost
 
 /*
-Sample Input:
-4
-1 5 7
-5 8 4
-3 2 9
-1 2 4
-
-Sample Output:
-8
-*/
-
+ * Sample Input:
+ * 4
+ * 1 5 7 (house 1)
+ * 5 8 4 (house 2)
+ * 3 2 9 (house 3)
+ * 1 2 4 (house 4)
+ * 
+ * Sample Output:
+ * 8
+ */
