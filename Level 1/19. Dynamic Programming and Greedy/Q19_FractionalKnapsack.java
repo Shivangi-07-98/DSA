c6771fd4499b48210437a82d1b6d10cc2c9a9729
Shivangi Kumari <12shivangikumari@gmail.com>
2025-11-19 07:42:15 +0530

    fractional knapsack

--- Level 1/19. Dynamic Programming and Greedy/Q19_FractionalKnapsack.java
+++ Level 1/19. Dynamic Programming and Greedy/Q19_FractionalKnapsack.java
@@ -1,76 +1,83 @@
 import java.util.*;
+
 public class Q19_FractionalKnapsack {
+
+  public static class Item implements Comparable<Item> {
+    int val;
+    int wt;
+    double vwRatio;
+
+    public int compareTo(Item o) {
+      if (this.vwRatio < o.vwRatio) {
+        return -1;
+      } else if (this.vwRatio > o.vwRatio) {
+        return +1;
+      } else {
+        return 0;
+      }
+    }
+  }
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
+    int n = scn.nextInt(); // 10
+
     int[] values = new int[n];
     int[] weights = new int[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // 33 14 50 9 8 11 6 40 2 15
       values[i] = scn.nextInt();
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // 7 2 5 9 3 2 1 10 3 3
       weights[i] = scn.nextInt();
     }
-    int capacity = scn.nextInt();
+    int cap = scn.nextInt(); // 5
 
-    System.out.println(fractionalKnapsack(values, weights, capacity));
+    System.out.println(fractionalKnapsack(values, weights, cap));
   }
 
   // Greedy solution
-  // Fractional knapsack can be solved using greedy approach
-  // Sort items by value/weight ratio and take items greedily
-  public static double fractionalKnapsack(int[] values, int[] weights, int capacity) {
+  public static double fractionalKnapsack(int[] values, int[] weights, int cap) {
     int n = values.length;
     Item[] items = new Item[n];
-    
+
     for (int i = 0; i < n; i++) {
-      items[i] = new Item(values[i], weights[i]);
+      items[i] = new Item();
+      items[i].val = values[i];
+      items[i].wt = weights[i];
+      items[i].vwRatio = (items[i].val * 1.0) / items[i].wt;
     }
-    
-    // Sort by value/weight ratio in descending order
-    Arrays.sort(items, (a, b) -> Double.compare(b.ratio, a.ratio));
-    
-    double totalValue = 0.0;
-    int remainingCapacity = capacity;
-    
-    for (Item item : items) {
-      if (remainingCapacity >= item.weight) {
-        // Take the whole item
-        totalValue += item.value;
-        remainingCapacity -= item.weight;
+
+    Arrays.sort(items);
+    double vib = 0; // value in bag
+    int rc = cap; // remaining capacity
+    int i = n - 1;
+
+    while (rc > 0) {
+      if (rc >= items[i].wt) {
+        vib = vib + items[i].val;
+        rc = rc - items[i].wt;
       } else {
-        // Take fraction of the item
-        totalValue += item.ratio * remainingCapacity;
+        vib += (rc * items[i].val * 1.0) / items[i].wt;
+        rc = 0;
         break;
       }
+      i--;
     }
-    
-    return totalValue;
+
+    return vib;
   }
-  
-  static class Item {
-    int value;
-    int weight;
-    double ratio;
-    
-    Item(int value, int weight) {
-      this.value = value;
-      this.weight = weight;
-      this.ratio = (double) value / weight;
-    }
-  }
-  
+
 }
+// greedy: sort by value/weight ratio, take items greedily
+// can take fraction of items
 
 /*
-Sample Input:
-3
-60 100 120
-10 20 30
-50
-
-Sample Output:
-240.0
-*/
-
+ * Sample Input:
+ * 10
+ * 33 14 50 9 8 11 6 40 2 15 (value)
+ * 7 2 5 9 3 2 1 10 3 3 (weight)
+ * 5 (bag of 5 kg capacity)
+ * 
+ * Sample Output:
+ * 50.0
+ */
