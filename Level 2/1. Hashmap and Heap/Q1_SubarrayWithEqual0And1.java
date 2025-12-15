b2eaad7837fe13109fa2cdee3c90d8f3868e9419
Shivangi Kumari <12shivangikumari@gmail.com>
2025-12-15 19:52:36 +0530

    subarray with equal number of 0 and 1

---
+++ Level 2/1. Hashmap and Heap/Q1_SubarrayWithEqual0And1.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Q1_SubarrayWithEqual0And1 {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+
+    int ans = countSubarrWithEqualZeroAndOne(arr, n);
+    System.out.println(ans);
+  }
+
+  // count subarrays having equal number of 0 and 1
+  static int countSubarrWithEqualZeroAndOne(int[] arr, int n) {
+    int ans = 0;
+    int sum = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+
+    for (int val : arr) {
+      sum += (val == 0 ? -1 : 1);
+
+      if (map.containsKey(sum)) {
+        ans += map.get(sum);
+        map.put(sum, map.get(sum) + 1);
+      } else {
+        map.put(sum, 1);
+      }
+    }
+
+    return ans;
+  }
+}
+
+/*
+Sample Input:
+7
+1 0 0 1 0 1 1
+
+Sample Output:
+8
+*/
