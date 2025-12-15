6497a4736e25cf8e7cc533a2aa81f8d59e6b67aa
Shivangi Kumari <12shivangikumari@gmail.com>
2025-12-15 16:28:57 +0530

    level 2 setup

---
+++ Level 2/1. Hashmap and Heap/Q1_Ques.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Q1_Ques {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    
+  }
+
+  
+}
+
+/*
+Sample Input:
+10
+
+Sample Output:
+55
+*/
