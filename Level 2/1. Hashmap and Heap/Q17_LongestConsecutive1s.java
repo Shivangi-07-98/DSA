e230ff2a9b8ec361d6119d0c555efa751d287a96
Shivangi Kumari <12shivangikumari@gmail.com>
2026-01-05 04:34:25 +0530

    longest consecutive sequence of 1's in a binary string

---
+++ Level 2/1. Hashmap and Heap/Q17_LongestConsecutive1s.java
@@ -0,0 +1,53 @@
+
+// LeetCode 485: Max Consecutive Ones
+// https://leetcode.com/problems/max-consecutive-ones/
+// Time Complexity: O(n), Space Complexity: O(1)
+import java.io.*;
+import java.util.*;
+
+public class Q17_LongestConsecutive1s {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+
+    int ans = longestConsecutiveOnes(arr);
+    System.out.println(ans);
+  }
+
+  static int longestConsecutiveOnes(int[] arr) {
+    int tl = 0;
+    int ml = 0;
+
+    for (int val : arr) {
+      if (val == 0) {
+        if (tl > ml) {
+          ml = tl;
+        }
+        tl = 0;
+      } else {
+        tl++;
+      }
+    }
+
+    if (tl > ml) {
+      ml = tl;
+    }
+
+    return ml;
+  }
+
+}
+
+/*
+ * Input:
+ * 8
+ * 1 1 0 1 1 1 0 1
+ * 
+ * Output:
+ * 3
+ */
