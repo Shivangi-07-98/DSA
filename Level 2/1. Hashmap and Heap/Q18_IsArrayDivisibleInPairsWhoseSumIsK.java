58b3d46f8af42b35d7f52e277bdd34b193c20a8d
Shivangi Kumari <12shivangikumari@gmail.com>
2026-01-05 02:00:39 +0530

    is array divisible in pairs whose sum is k

---
+++ Level 2/1. Hashmap and Heap/Q18_IsArrayDivisibleInPairsWhoseSumIsK.java
@@ -0,0 +1,68 @@
+// Time Complexity: O(n), Space Complexity: O(n)
+
+// WHAT IS THE QUESTION ASKING?
+// Check if array can be divided into pairs such that sum of each pair is divisible by k
+import java.io.*;
+import java.util.*;
+
+public class Q18_IsArrayDivisibleInPairsWhoseSumIsK {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int k = scn.nextInt();
+
+    solution(arr, k);
+  }
+
+  public static void solution(int[] arr, int k) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int val : arr) {
+      int rem = val % k;
+      if (map.containsKey(rem)) {
+        map.put(rem, map.get(rem) + 1);
+      } else {
+        map.put(rem, 1);
+      }
+    }
+
+    for (int val : arr) {
+      int rem = val % k;
+
+      if (2 * rem == k) {
+        if (map.get(rem) % 2 != 0) {
+          System.out.println("false");
+          return;
+        }
+      } else if (rem == 0) {
+        if (map.get(rem) % 2 != 0) {
+          System.out.println("false");
+          return;
+        }
+      } else {
+        if (map.get(rem) != map.get(k - rem)) {
+          System.out.println("false");
+          return;
+        }
+      }
+    }
+
+    System.out.println("true");
+  }
+
+}
+
+/*
+ * Input:
+ * 4
+ * 9 7 5 3
+ * 6
+ * 
+ * Output:
+ * true
+ */
