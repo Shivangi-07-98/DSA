8b9129ccde71e8dfc6e799261b451bed854937ad
Shivangi Kumari <12shivangikumari@gmail.com>
2026-01-20 00:43:08 +0530

    Object creation

--- System Design/OOPs/f3.java
+++ System Design/OOPs/f3.java
@@ -8,9 +8,16 @@ class f3 {
     Student s = new Student(10, "A");
   }
 
+  public class DBConnection {
+  }
+
+  public class Directory {
+  }
+
   static class Student {
     // During space allocation all data members are set to JAVA given default values
-    // Parsing sets data members to Class creator's defaults (e.g. maxxScore and schoolName here and growthMultiple in ArrayList)
+    // Parsing sets data members to Class creator's defaults (e.g. maxScore and
+    // schoolName here and growthMultiple in ArrayList)
     int age;
     String name;
     String house;
@@ -22,29 +29,29 @@ class f3 {
 
     // static block - for setting dynamic kind of static properties
     static {
-        // kind of default ctor for static properties 
-        maxScore = read from database and set;
+      // kind of default ctor for static properties
+      // maxScore = read from database and set;
     }
 
-    // For class creator to initialize data members which are dynamic e.g. reading from a file or a db
-    Student(){
-        dbconnection = new DBConnection("<ip address>");
-        studentFolder = Files.read("..");
-        fees = dbconnection.getFees();
+    // For class creator to initialize data members which are dynamic e.g. reading
+    // from a file or a db
+    Student() {
+      // dbconnection = new DBConnection("<ip address>");
+      // studentFolder = Files.read("..");
+      // fees = dbconnection.getFees();
     }
 
     // For class user to set values to data members
-    Student(int age, String name){
-        this(); // constructor chaining, if give always first line
-        this.age = age;
-        this.name = name;
+    Student(int age, String name) {
+      this(); // constructor chaining, if give always first line
+      this.age = age;
+      this.name = name;
     }
 
-    Student(Student s){
+    Student(Student s) {
       this.age = s.age;
       this.name = s.name;
     }
-    
   }
 
 }
