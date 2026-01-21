ce29a1f48dcf9cad667e30a584b130c51e50b509
Shivangi Kumari <12shivangikumari@gmail.com>
2026-01-22 00:29:25 +0530

    find all anagrams

--- Level 2/1. Hashmap and Heap/Q19_FindAllAnagrams.java
+++ Level 2/1. Hashmap and Heap/Q19_FindAllAnagrams.java
@@ -11,9 +11,64 @@ public class Q19_FindAllAnagrams {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    
+    String s = scn.next();
+    String p = scn.next();
+    findAnagrams(s, p);
   }
 
+  public static void findAnagrams(String s, String p) {
+    int[] map2 = new int[26];
+    for (int i = 0; i < p.length(); i++) {
+      char ch = p.charAt(i);
+      map2[ch - 'a']++;
+    }
+
+    int[] map1 = new int[26];
+    for (int i = 0; i < p.length(); i++) {
+      char ch = s.charAt(i);
+      map1[ch - 'a']++;
+    }
+
+    ArrayList<Integer> res = new ArrayList<>();
+    if (areAnagram(map1, map2)) {
+      res.add(0);
+    }
+
+    int i = p.length();
+    int j = 0;
+
+    while (i < s.length()) {
+      char chi = s.charAt(i);
+      map1[chi - 'a']++;
+
+      char chj = s.charAt(j);
+      if (map1[chj - 'a'] > 0) {
+        map1[chj - 'a']--;
+      }
+
+      if (areAnagram(map1, map2) == true) {
+        res.add(j + 1);
+      }
+
+      i++;
+      j++;
+    }
+
+    System.out.println(res.size());
+    for (int val : res) {
+      System.out.print(val + " ");
+    }
+  }
+
+  // Constant time complexity
+  public static boolean areAnagram(int[] map1, int[] map2) {
+    for (int i = 0; i < 26; i++) {
+      if (map1[i] != map2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 /*
