c31f7219313bc1075a85909a17c9eb3a3087c8ed
Shivangi Kumari <12shivangikumari@gmail.com>
2026-02-18 02:29:20 +0530

    minimum stack 1

--- Level 1/18. Stacks and Queues/Q19_MinimumStack1.java
+++ Level 1/18. Stacks and Queues/Q19_MinimumStack1.java
@@ -2,8 +2,114 @@ import java.io.*;
 import java.util.*;
 
 public class Q19_MinimumStack1 {
-  public static void main(String[] args) {
+
+  public static class MinStack {
+    Stack<Integer> allData;
+    Stack<Integer> minData;
+
+    public MinStack() {
+      allData = new Stack<>();
+      minData = new Stack<>();
+    }
+
+    int size() {
+      return allData.size();
+    }
+
+    void push(int val) {
+      allData.push(val);
+
+      if (minData.size() == 0 || val <= minData.peek()) {
+        minData.push(val);
+      }
+    }
+
+    int pop() {
+      if (allData.size() == 0) {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+
+      if (allData.peek().equals(minData.peek())) {
+        int val = allData.pop();
+        minData.pop();
+        return val;
+      } else {
+        return allData.pop();
+      }
+    }
+
+    int top() {
+      if (allData.size() == 0) {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      return allData.peek();
+    }
+
+    int min() {
+      if (minData.size() == 0) {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      return minData.peek();
+    }
     
   }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    MinStack st = new MinStack();
+
+    while (true) {
+      String str = scn.nextLine();
+
+      if (str.equals("quit")) {
+        break;
+      } else if (str.startsWith("push")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if (str.startsWith("pop")) {
+        int val = st.pop();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("top")) {
+        int val = st.top();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(st.size());
+      } else if (str.startsWith("min")) {
+        int val = st.min();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      }
+    }
+
+    scn.close();
+  }
+
 }
 
+/*
+ * INPUT:
+ * push 10
+ * push 20
+ * push 5
+ * min
+ * pop
+ * min
+ * top
+ * size
+ * quit
+ * 
+ * OUTPUT:
+ * 5
+ * 5
+ * 10
+ * 20
+ * 2
+ */
