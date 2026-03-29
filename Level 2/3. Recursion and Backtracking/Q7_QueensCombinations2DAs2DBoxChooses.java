ed8306e97b8182f0878d49c0d02e5a3cca524fb6
Shivangi Kumari <12shivangikumari@gmail.com>
2026-03-30 03:30:57 +0530

    q7

--- Level 2/3. Recursion and Backtracking/Q7_QueensCombinations2DAs2DBoxChooses.java
+++ Level 2/3. Recursion and Backtracking/Q7_QueensCombinations2DAs2DBoxChooses.java
@@ -7,69 +7,48 @@ public class Q7_QueensCombinations2DAs2DBoxChooses {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    int m = scn.nextInt();
-    int tq = scn.nextInt();
-    int[][] board = new int[n][m];
-    queens(0, 0, tq, board);
+    queens(0, n, 0, 0, "");
   }
 
-  private static void queens(int idx, int qpsf, int tq, int[][] board) {
-    int n = board.length;
-    int m = board[0].length;
-    if (idx == n * m) {
+  public static void queens(int qpsf, int tq, int row, int col, String asf) {
+    if (row == tq) {
       if (qpsf == tq) {
-        printBoard(board);
+        System.out.println(asf);
       }
       return;
     }
 
-    int r = idx / m;
-    int c = idx % m;
-
-    if (qpsf < tq) {
-      board[r][c] = qpsf + 1;
-      queens(idx + 1, qpsf + 1, tq, board);
-      board[r][c] = 0;
+    if (col != tq - 1) {
+      queens(qpsf + 1, tq, row + 1, 0, asf + "q\n");
+      queens(qpsf + 0, tq, row + 1, 0, asf + "-\n");
+    } else {
+      queens(qpsf + 1, tq, row, col + 1, asf + "q");
+      queens(qpsf + 0, tq, row, col + 1, asf + "-");
     }
-
-    queens(idx + 1, qpsf, tq, board);
   }
-
-  private static void printBoard(int[][] board) {
-    for (int[] row : board) {
-      for (int val : row) {
-        System.out.print(val == 0 ? "-\t" : "q" + val + "\t");
-      }
-      System.out.println();
-    }
-    System.out.println();
-  }
-
 
 }
 
 /*
  * Input:
  * 2
- * 2
- * 2
  *
  * Output:
- * q1	q2	
- * -	-	
+ * q1 q2
+ * - -
  *
- * q1	-	
- * q2	-	
+ * q1 -
+ * q2 -
  *
- * q1	-	
- * -	q2	
+ * q1 -
+ * - q2
  *
- * -	q1	
- * q2	-	
+ * - q1
+ * q2 -
  *
- * -	q1	
- * -	q2	
+ * - q1
+ * - q2
  *
- * -	-	
- * q1	q2	
+ * - -
+ * q1 q2
  */
