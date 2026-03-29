eba63bb071cb04afd5e004d3cf03c371093b9d17
Shivangi Kumari <12shivangikumari@gmail.com>
2026-03-30 00:54:53 +0530

    q4

--- Level 2/3. Recursion and Backtracking/Q3_Combinations1.java
+++ Level 2/3. Recursion and Backtracking/Q3_Combinations1.java
@@ -3,6 +3,14 @@
 import java.util.*;
 public class Q3_Combinations1 {
 
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int nboxes = scn.nextInt(); // 3
+    int ritems = scn.nextInt(); // 2
+    combinations(1, nboxes, 0, ritems, "");
+  }
+
+  // Box chooses take/skip (combination)
   public static void combinations(int cb, int tb, int ssf, int ts, String asf) {
     if (cb > tb) {
       if (ssf == ts) {
@@ -11,22 +19,18 @@ public class Q3_Combinations1 {
       return;
     }
 
-    if (ssf < ts) {
+    // if (ssf < ts) {
       combinations(cb + 1, tb, ssf + 1, ts, asf + "i");
-    }
+    // }
     combinations(cb + 1, tb, ssf, ts, asf + "_");
-  }
-
-  public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int nboxes = scn.nextInt();
-    int ritems = scn.nextInt();
-    combinations(1, nboxes, 0, ritems, "");
   }
 
 }
 
 /*
+ * Q3 (box chooses take/skip)
+ * Order does not matter
+ *
  * Input:
  * 3
  * 2
--- Level 2/3. Recursion and Backtracking/Q4_Combinations2.java
+++ Level 2/3. Recursion and Backtracking/Q4_Combinations2.java
@@ -1,9 +1,16 @@
 // Time Complexity: O(nCr * n)
 // Space Complexity: O(n)
 import java.util.*;
-
 public class Q4_Combinations2 {
 
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int nboxes = scn.nextInt(); // 3
+    int ritems = scn.nextInt(); // 2
+    combinations(new int[nboxes], 1, ritems, -1);
+  }
+
+  // Item chooses box (only forward)
   public static void combinations(int[] boxes, int ci, int ti, int lb) {
     if (ci > ti) {
       for (int val : boxes) {
@@ -24,18 +31,16 @@ public class Q4_Combinations2 {
         boxes[i] = 0;
       }
     }
-  }
-
-  public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int nboxes = scn.nextInt();
-    int ritems = scn.nextInt();
-    combinations(new int[nboxes], 1, ritems, -1);
+    
   }
 
 }
 
 /*
+ * Q4 (item chooses box, only forward)
+ * Where should item 1 go? item 2 goes only after that box.
+ * Order does not matter, so we move forward only.
+ *
  * Input:
  * 3
  * 2
