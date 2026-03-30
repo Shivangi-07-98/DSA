0477ca85379c656bbe2c0f5a04719255a527ff21
Shivangi Kumari <12shivangikumari@gmail.com>
2026-03-30 20:41:27 +0530

    q11

--- Level 2/3. Recursion and Backtracking/Q11_NQueensPermutations2DAs1DQueenChooses.java
+++ Level 2/3. Recursion and Backtracking/Q11_NQueensPermutations2DAs1DQueenChooses.java
@@ -7,63 +7,59 @@ public class Q11_NQueensPermutations2DAs1DQueenChooses {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    int tq = n;
-    int[][] board = new int[n][n];
-    nqueens(1, tq, board);
+    int[][] chess = new int[n][n];
+    nqueens(0, n, chess);
   }
 
-  private static void nqueens(int qpsf, int tq, int[][] board) {
-    if (qpsf > tq) {
-      printBoard(board);
+  private static void nqueens(int qpsf, int tq, int[][] chess) {
+    if (qpsf == tq) {
+      printBoard(chess);
       return;
     }
 
-    int n = board.length;
-    int m = board[0].length;
+    int n = chess.length;
+    int m = chess[0].length;
     for (int i = 0; i < n * m; i++) {
       int r = i / m;
       int c = i % m;
-      if (board[r][c] == 0 && isSafe(board, r, c)) {
-        board[r][c] = qpsf;
-        nqueens(qpsf + 1, tq, board);
-        board[r][c] = 0;
+      if (chess[r][c] == 0 && isSafe(chess, r, c)) {
+        chess[r][c] = qpsf + 1;
+        nqueens(qpsf + 1, tq, chess);
+        chess[r][c] = 0;
       }
     }
   }
 
-  private static boolean isSafe(int[][] board, int r, int c) {
-    int n = board.length;
-
-    for (int i = r - 1; i >= 0; i--) {
-      if (board[i][c] != 0) return false;
+  public static boolean isSafe(int[][] chess, int row, int col) {
+    for (int i = row - 1, j = col; i >= 0; i--) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int i = r + 1; i < n; i++) {
-      if (board[i][c] != 0) return false;
+    for (int i = row + 1, j = col; i < chess.length; i++) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int j = c - 1; j >= 0; j--) {
-      if (board[r][j] != 0) return false;
+    for (int i = row, j = col - 1; j >= 0; j--) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int j = c + 1; j < n; j++) {
-      if (board[r][j] != 0) return false;
+    for (int i = row, j = col + 1; j < chess.length; j++) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
-      if (board[i][j] != 0) return false;
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++) {
-      if (board[i][j] != 0) return false;
+    for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int i = r + 1, j = c - 1; i < n && j >= 0; i++, j--) {
-      if (board[i][j] != 0) return false;
+    for (int i = row + 1, j = col + 1; i < chess.length && j < chess.length; i++, j++) {
+      if (chess[i][j] != 0) return false;
     }
-    for (int i = r + 1, j = c + 1; i < n && j < n; i++, j++) {
-      if (board[i][j] != 0) return false;
+    for (int i = row + 1, j = col - 1; i < chess.length && j >= 0; i++, j--) {
+      if (chess[i][j] != 0) return false;
     }
-
     return true;
   }
 
-  private static void printBoard(int[][] board) {
-    for (int[] row : board) {
+  private static void printBoard(int[][] chess) {
+    for (int[] row : chess) {
       for (int val : row) {
         System.out.print(val == 0 ? "-\t" : "q" + val + "\t");
       }
@@ -72,13 +68,32 @@ public class Q11_NQueensPermutations2DAs1DQueenChooses {
     System.out.println();
   }
 
-
 }
 
 /*
  * Input:
- * 1
+ * 4
  *
  * Output:
- * q1	
+ * -	q1	-	-	
+ * -	-	-	q2	
+ * q3	-	-	-	
+ * -	-	q4	-	
+ *
+ * -	q1	-	-	
+ * -	-	-	q2	
+ * q4	-	-	-	
+ * -	-	q3	-	
+ *
+ * -	q1	-	-	
+ * -	-	-	q3	
+ * q2	-	-	-	
+ * -	-	q4	-	
+ *
+ * -	q1	-	-	
+ * -	-	-	q4	
+ * q2	-	-	-	
+ * -	-	q3	-	
+ * 
+ * ... 44 more boards
  */
