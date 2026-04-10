064ef4acc1904209046960b3c501270b0edf2d8b
Shivangi Kumari <12shivangikumari@gmail.com>
2026-04-10 17:18:30 +0530

    q19

--- Level 2/1. Hashmap and Heap/Q19_FindAllAnagrams.java
+++ Level 2/1. Hashmap and Heap/Q19_FindAllAnagrams.java
@@ -11,22 +11,22 @@ public class Q19_FindAllAnagrams {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    String s = scn.next();
-    String p = scn.next();
-    findAnagrams(s, p);
+    String s1 = scn.next();
+    String s2 = scn.next();
+    findAnagrams(s1, s2);
   }
 
-  public static void findAnagrams(String s, String p) {
-    int[] map2 = new int[26];
-    for (int i = 0; i < p.length(); i++) {
-      char ch = p.charAt(i);
-      map2[ch - 'a']++;
+  public static void findAnagrams(String s1, String s2) {
+    int[] map1 = new int[26];
+    for (int i = 0; i < s2.length(); i++) {
+      char ch = s1.charAt(i);
+      map1[ch - 'a']++;
     }
 
-    int[] map1 = new int[26];
-    for (int i = 0; i < p.length(); i++) {
-      char ch = s.charAt(i);
-      map1[ch - 'a']++;
+    int[] map2 = new int[26];
+    for (int i = 0; i < s2.length(); i++) {
+      char ch = s2.charAt(i);
+      map2[ch - 'a']++;
     }
 
     ArrayList<Integer> res = new ArrayList<>();
@@ -34,14 +34,14 @@ public class Q19_FindAllAnagrams {
       res.add(0);
     }
 
-    int i = p.length();
+    int i = s2.length();
     int j = 0;
 
-    while (i < s.length()) {
-      char chi = s.charAt(i);
+    while (i < s1.length()) {
+      char chi = s1.charAt(i);
       map1[chi - 'a']++;
 
-      char chj = s.charAt(j);
+      char chj = s1.charAt(j);
       map1[chj - 'a']--;
 
       if (areAnagram(map1, map2) == true) {
