7cdc8430f67d8606723dd01a9956a05b0b48688b
Shivangi Kumari <12shivangikumari@gmail.com>
2026-04-12 19:52:11 +0530

    q27

--- Level 2/1. Hashmap and Heap/Q27_IsomorphicStrings.java
+++ Level 2/1. Hashmap and Heap/Q27_IsomorphicStrings.java
@@ -9,18 +9,14 @@ import java.io.*;
 import java.util.*;
 
 public class Q27_IsomorphicStrings {
-
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-
     String s1 = scn.next();
     String s2 = scn.next();
-
     System.out.println(solution(s1, s2));
   }
 
   public static boolean solution(String s1, String s2) {
-
     if (s1.length() != s2.length())
       return false;
 
@@ -28,24 +24,19 @@ public class Q27_IsomorphicStrings {
     HashMap<Character, Boolean> used = new HashMap<>();
 
     for (int i = 0; i < s1.length(); i++) {
-
       char ch1 = s1.charAt(i);
       char ch2 = s2.charAt(i);
 
       // Mapping exists
       if (map.containsKey(ch1)) {
-
         if (map.get(ch1) != ch2) {
           return false; // conflict
         }
-
       } else {
-
         // Reverse conflict
         if (used.containsKey(ch2)) {
           return false;
         }
-
         map.put(ch1, ch2);
         used.put(ch2, true);
       }
