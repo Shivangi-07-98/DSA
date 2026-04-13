9ea3c71a823ba938ef7cc082e657e5f4ac23b0cd
Shivangi Kumari <12shivangikumari@gmail.com>
2026-04-13 23:46:23 +0530

    q16

---
+++ Level 2/7. Dynamic Programming/Q15_PaintFence.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Q15_PaintFence {
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int k = scn.nextInt();
+
+    int ii = k * 1;
+    int ij = k * (k - 1);
+    int tot = ii + ij;
+
+    for (int i = 3; i <= n; i++) {
+      ii = ij * 1;
+      ij = tot * (k - 1);
+      tot = ii + ij;
+    }
+
+    System.out.println(tot);
+  }
+}
