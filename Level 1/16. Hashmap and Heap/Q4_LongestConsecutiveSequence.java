72bd7830fa3664bdf16dfc36b3261e23173020ca
Shivangi Kumari <12shivangikumari@gmail.com>
2026-04-30 13:24:39 +0530

    q4

--- Level 1/16. Hashmap and Heap/Q4_LongestConsecutiveSequence.java
+++ Level 1/16. Hashmap and Heap/Q4_LongestConsecutiveSequence.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
 import java.io.*;
 import java.util.*;
 
@@ -51,3 +53,40 @@ public class Q4_LongestConsecutiveSequence {
   }
 
 }
+
+// ---------------------------------------------------------------------
+
+/*
+class Solution {
+  public int longestConsecutive(int[] nums) {
+    HashMap<Integer, Boolean> map = new HashMap<>();
+    for (int val : nums) {
+      map.put(val, true);
+    }
+
+    for (int val : nums) {
+      if (map.containsKey(val - 1)) {
+        map.put(val, false);
+      }
+    }
+
+    int res = 0;
+
+    for (int val : nums) {
+      if (map.get(val) == true) {
+        int len = 1;
+        while (map.containsKey(val + len)) {
+          len++;
+        }
+
+        if (len > res) {
+          res = len;
+        }
+      }
+    }
+
+    return res;
+  }
+}
+
+*/
