2179b9d257589060d03561da145d36a317ba36bd
Shivangi Kumari <12shivangikumari@gmail.com>
2026-06-15 10:17:09 +0530

    q27

--- Level 2/1. Hashmap and Heap/Q27_IsomorphicStrings.java
+++ Level 2/1. Hashmap and Heap/Q27_IsomorphicStrings.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/isomorphic-strings/
+
 // Time O(n)
 // Space O(n)
 
@@ -28,13 +30,11 @@ public class Q27_IsomorphicStrings {
       char ch1 = s1.charAt(i);
       char ch2 = s2.charAt(i);
 
-      // Mapping exists
       if (map.containsKey(ch1)) {
         if (map.get(ch1) != ch2) {
-          return false; // conflict
+          return false;
         }
       } else {
-        // Reverse conflict
         if (used.containsKey(ch2)) {
           return false;
         }
