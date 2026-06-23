78784c58757320a6c834509e8a982872261def7e
Shivangi Kumari <12shivangikumari@gmail.com>
2026-06-23 17:05:38 +0530

    gap

--- Level 2/1. Hashmap and Heap/Q32_FirstNonRepeatingCharacter.java
+++ Level 2/1. Hashmap and Heap/Q32_FirstNonRepeatingCharacter.java
@@ -12,9 +12,11 @@ public class Q32_FirstNonRepeatingCharacter {
 
   public static char solution(String str) {
     int[] freq = new int[256];
+
     for (int i = 0; i < str.length(); i++) {
       freq[str.charAt(i)]++;
     }
+
     for (int i = 0; i < str.length(); i++) {
       if (freq[str.charAt(i)] == 1) {
         return str.charAt(i);
