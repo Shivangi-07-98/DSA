07c4331fbc6ac74997b2daae42ef56beb2d00ad4
Shivangi Kumari <12shivangikumari@gmail.com>
2026-06-24 23:32:46 +0530

    q4

--- Level 2/1. Hashmap and Heap/Q4_SubarraySumDivisibleByK.java
+++ Level 2/1. Hashmap and Heap/Q4_SubarraySumDivisibleByK.java
@@ -56,3 +56,26 @@ public class Q4_SubarraySumDivisibleByK {
 // 
 // Output:
 // 7
+
+/* note - don't format
+
+Array:
+     3   -42    17    11    23    31     2    17
+
+Sum:
+0    3   -39   -22   -11    12    43    45    62
+
+Remainder (k = 7):
+0₁   3₁    3₂    6₁    3₃    5₁    1₁    3₄    6₂
+
+
+-22 = (-21) - 1
+    = (-28) + 6
+
+Therefore:
+-22 % 7 = -1
+
+Adjusted remainder:
+-1 + 7 = 6
+
+*/
