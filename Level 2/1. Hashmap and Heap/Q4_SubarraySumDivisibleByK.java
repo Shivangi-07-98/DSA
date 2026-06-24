8f475dcdb834e7ff116b47d5d1a734446dea63af
Shivangi Kumari <12shivangikumari@gmail.com>
2026-06-24 20:47:28 +0530

    q4

--- Level 2/1. Hashmap and Heap/Q4_SubarraySumDivisibleByK.java
+++ Level 2/1. Hashmap and Heap/Q4_SubarraySumDivisibleByK.java
@@ -7,7 +7,6 @@ import java.io.*;
 import java.util.*;
 
 public class Q4_SubarraySumDivisibleByK {
-
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
