53fae59b1db918a18fb742ae7ec3f3eef8c2acc2
Shivangi Kumari <12shivangikumari@gmail.com>
2026-07-08 23:25:28 +0530

    gap

--- Level 2/1. Hashmap and Heap/Q53_PairsWithEqualSum.java
+++ Level 2/1. Hashmap and Heap/Q53_PairsWithEqualSum.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.*;
 
 public class Q53_PairsWithEqualSum {
+
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
