6e6d3f519bc08545853f915b3eb70ae75de8e7d6
Shivangi Kumari <12shivangikumari@gmail.com>
2026-08-17 17:10:28 +0530

    f10

--- Core/OOPs/f10.java
+++ Core/OOPs/f10.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class f10 {
-  
   public static void main(String[] args) throws NumberFormatException, IOException {
     Employee obj = new Employee("A", 10); // not allowed because Employee class is abstract
     obj.markAttendance();
@@ -14,34 +13,34 @@ public class f10 {
 
     obj = new SoftwareEmployee("", "A", 10);
     obj.markAttendance();
-
   }
 
   static abstract class Employee {
     String name;
     int empid;
 
-    Employee(String name, int empid){
+    Employee(String name, int empid) {
       this.name = name;
       this.empid = empid;
     }
 
-    final void markAttendance(){
+    // final
+    void markAttendance() {
       System.out.println(name + " is present");
     }
 
-    abstract void doWork();
+    // abstract void doWork();
   }
 
   static class HREmployee extends Employee {
     List<Integer> salaries;
 
-    HREmployee(List<Integer> salaries, String name, int empid){
+    HREmployee(List<Integer> salaries, String name, int empid) {
       super(name, empid);
       this.salaries = salaries;
     }
 
-    void doWork(){
+    void doWork() {
       System.out.println(name + " creates payroll");
     }
   }
@@ -49,30 +48,40 @@ public class f10 {
   static class SoftwareEmployee extends Employee {
     String conn;
 
-    SoftwareEmployee(String conn, String name, int empid){
+    SoftwareEmployee(String conn, String name, int empid) {
       super(name, empid);
       this.conn = conn;
     }
 
-    void doWork(){
+    void doWork() {
       System.out.println(name + " writes code");
     }
   }
 
-  static class OperationsEmployee exends Employee {
-    List<Integer> operations;
+  // static class OperationsEmployee extends Employee {
+  // List<Integer> operations;
 
-    SoftwareEmployee(List<Integer> ops, String name, int empid){
-      super(name, empid);
-      this.operations = ops;
-    }
+  // SoftwareEmployee(List<Integer> ops, String name, int empid){
+  // super(name, empid);
+  // this.operations = ops;
+  // }
 
-    void doWork(){
-      System.out.println(name + " does operations");
-    }
-  }
-
-  
-  
+  // void doWork() {
+  // System.out.println(name + " does operations");
+  // }
+  // }
 
 }
+
+// super(name, empid);
+// means call the constructor of my parent class (Employee) and give it name and
+// empid
+
+// class HREmployee extends Employee {
+//   int salary;
+
+//   HREmployee(String name, int empid, int salary) {
+//     super(name, empid); // Parent handles name and empid
+//     this.salary = salary; // Child handles salary
+//   }
+// }
